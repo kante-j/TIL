533d187c5770e3db193ac450b2f8eb53163d08b0
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-12-18 17:34:40 +0900

    1759 암호 만들기

--- 백준/1759 암호 만들기.py
+++ 백준/1759 암호 만들기.py
@@ -1,14 +1,22 @@
 import itertools
 from sys import stdin
 L,C = map(int, input().split())
-li = list(map(stdin.readline().split()))
+li = list((stdin.readline().split()))
 arr = []
 for i in li:
     arr.append(i)
 arr.sort()
-
-def backtracking(n, m):
-    if n==m:
-        print()
-        return
-    for
+mo = ['a','e','i','o','u']
+for i in list(itertools.combinations(arr,L)):
+    res = ""
+    for j in i:
+        res += j
+    for m in mo:
+        if m in res:
+            cnt = 0
+            for c in res:
+                if c not in mo:
+                    cnt += 1
+            if cnt >= 2:
+                print(res)
+                break
